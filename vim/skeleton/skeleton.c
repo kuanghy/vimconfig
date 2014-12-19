ecbd23cb4f74a1b81a5bbf1322a8e99f8d7d304c
konghy <sudohuoty@163.com>
2014-12-19 22:05:19 +0800

    6.8.1

---
+++ vim/skeleton/skeleton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/*************************************************************************
+ * FuncName: main
+ *    Input: None
+ *   Output: None
+ *  Val-Res: None
+ *   Return: 0
+ *    Brief: Main program body!
+*************************************************************************/
+int main(void)
+{
+	
+
+    return 0;
+}
+
+
+/************************ (C) COPYRIGHT HOUTY PRIVATE ********END OF FILE****/
