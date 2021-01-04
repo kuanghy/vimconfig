97791daee45bd725a99fbf6a7376927429e3a8f1
Huayong Kuang <sudohuoty@163.com>
2021-01-04 18:44:36 +0800

    支持新建go语言源文件时载入骨架

---
+++ vim-pro/skeleton/skeleton.go
@@ -0,0 +1,12 @@
+:insert
+// Copyright (c) Huoty, All rights reserved
+// Author: Huoty <sudohuoty@163.com>
+// CreateTime:
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("")
+}
--- vim-pro/skeleton/skeleton.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Copyright (c) JoinQuant Development Team
-# Author: Huayong Kuang <kuanghuayong@joinquant.com>
-
